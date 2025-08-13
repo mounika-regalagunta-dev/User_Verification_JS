6bd08fc75ab905e1373aba84644ffc499018a885
Mounika Regalagunta <mounikaregalagunta@gmail.com>
2025-08-13 14:21:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function onHandleSubmit(){
+    let userName=document.getElementById("name").value
+    document.getElementById("message").textContent="✨ Hang on, " + userName + "! Your account is almost ready."
+}
